cce338de66109fdf95b1562cb66c7af6a700720e
aishwarya1505 <43162654+aishwarya1505@users.noreply.github.com>
2018-09-11 10:36:26 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,10 @@
+a=int(input("enter value of A: "))
+b=int(input("enter value of b: "))
+print("A:,a")
+print("B:,b")
+if(a>b):
+	print(a,"is greater")
+else:
+	print(b,"is greater")
+
+
---
+++ 5.py
@@ -0,0 +1,10 @@
+a=int(input("enter value of A: "))
+b=int(input("enter value of b: "))
+print("A:,a")
+print("B:,b")
+if(a<b):
+	print(a,"is lesser")
+else:
+	print(b,"is lesser")
+
+
