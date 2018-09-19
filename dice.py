687457a02daee62f1a1b8915d8b52c50a74a2c31
aishwarya1505 <43162654+aishwarya1505@users.noreply.github.com>
2018-09-19 14:40:41 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -2,18 +2,3 @@ import random
 print(random.randint(1,6))
 
 
-========================
-OUTPUT:
-cl316@soetcse:~/aishwarya$ python3 dice.py
-2
-cl316@soetcse:~/aishwarya$ 
-cl316@soetcse:~/aishwarya$ python3 dice.py
-3
-cl316@soetcse:~/aishwarya$ python3 dice.py
-4
-cl316@soetcse:~/aishwarya$ python3 dice.py
-3
-cl316@soetcse:~/aishwarya$ python3 dice.py
-5
-cl316@soetcse:~/aishwarya$ python3 dice.py
-
