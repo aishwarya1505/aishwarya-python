74fa3a1923bacb725050c980615018986fd4cae5
aishwarya1505 <43162654+aishwarya1505@users.noreply.github.com>
2018-10-09 09:59:31 +0530

    Update sos.py

--- sos.py
+++ sos.py
@@ -23,6 +23,91 @@ for  in range(9):
 		if p in a:
 			a[int(p)-1]= '0'
 			playerOneTurn = not playerOneTurn
+			------------------------------------------
+	output
+	
+1|2|3
+-------
+4|5|6
+-------
+7|8|9
+-------
+player 1, choose your palce:2
+1|x|3
+-------
+4|5|6
+-------
+7|8|9
+-------
+player 2, choose your place:7
+1|x|3
+-------
+4|5|6
+-------
+0|8|9
+-------
+player 1, choose your palce:5
+1|x|3
+-------
+4|x|6
+-------
+0|8|9
+-------
+player 2, choose your place:6
+1|x|3
+-------
+4|x|0
+-------
+0|8|9
+-------
+player 1, choose your palce:1
+x|x|3
+-------
+4|x|0
+-------
+0|8|9
+-------
+player 2, choose your place:1
+x|x|3
+-------
+4|x|0
+-------
+0|8|9
+-------
+player 2, choose your place:4
+x|x|3
+-------
+0|x|0
+-------
+0|8|9
+-------
+player 1, choose your palce:2
+x|x|3
+-------
+0|x|0
+-------
+0|8|9
+-------
+player 1, choose your palce:
+cl316@soetcse:~/aishwarya$ python3 sos.py
+1|2|3
+-------
+4|5|6
+-------
+7|8|9
+-------
+player 1, choose your palce:4
+1|2|3
+-------
+x|5|6
+-------
+7|8|9
+-------
+player 2, choose your place:3
+1|2|0
+-------
+x|5|6
+-------
 
 
 
