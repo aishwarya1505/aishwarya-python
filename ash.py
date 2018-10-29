6c36307eea76d02c5b34d20b3780dadd4a954261
aishwarya1505 <43162654+aishwarya1505@users.noreply.github.com>
2018-10-29 11:24:43 +0530

    Add files via upload

---
+++ ash.py
@@ -0,0 +1,11 @@
+import smtplib
+import getpass
+myemail=input("your email id:")
+password=getpass.getpass()
+recemail=input("reciver's email id:")
+s=smtplib.SMTP('smtp.gmail.com',587)
+s.starttls()
+s.login(myemail,password)
+message= "hi how are you"
+s.sendmail(mymail,recemail,message)
+s.quit()
